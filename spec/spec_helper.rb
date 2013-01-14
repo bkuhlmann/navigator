f07ab4e32185774baa91c4599fdaa61e3d245ace
Brooke Kuhlmann <brooke@redalchemist.com>
2013-01-13 17:04:12 -0700

    Added spec focus capability. [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,6 @@ require "navigator"
 require "navigator/action_view/instance_methods"
 
 RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
