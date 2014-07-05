1c76bfe8d4333435263717f86a5e44e1b3bf8d62
Brooke Kuhlmann <brooke@redalchemist.com>
2014-07-05 09:09:01 -0600

    Added active selector support for tags.
    * Allows tags to be marked as "active" when search and target values
      match.
    * Usage: a "Example", {href: "/example"}, {search_value: "/example"}.
    * Yields: <a href="/example" class="active"></a>

--- lib/navigator/tag.rb
+++ lib/navigator/tag.rb
@@ -1,10 +1,16 @@
 module Navigator
   # Renders a HTML tag.
   class Tag
-    attr_reader :name, :content, :attributes
+    attr_reader :name, :content
 
-    def initialize name, content = nil, attributes = {}
-      @name, @content, @attributes = name, content, attributes
+    def initialize name, content = nil, attributes = {}, settings = {}
+      @name = name
+      @content = content
+      @attributes = attributes.with_indifferent_access
+      @settings = settings.with_indifferent_access.reverse_merge search_key: :href,
+                                                                 search_value: nil,
+                                                                 target_key: :class,
+                                                                 target_value: "active"
     end
 
     def prefix
@@ -21,7 +27,21 @@ module Navigator
 
     private
 
+    attr_reader :attributes, :settings
+
+    def activate_attribute
+      search_key = settings.fetch :search_key
+      search_value = settings.fetch :search_value
+
+      if search_value.present? && attributes[search_key] == search_value
+        target_key = settings.fetch :target_key
+        target_value = settings.fetch :target_value
+        attributes[target_key] = [attributes[target_key], target_value].compact * ' '
+      end
+    end
+
     def formatted_attributes
+      activate_attribute
       attrs = attributes.map { |key, value| %(#{key}="#{value}") } * ' '
       attrs.empty? ? nil : " #{attrs}"
     end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Navigator::Tag do
-  let(:tag) { Navigator::Tag.new "li", "Not much here.", class: "example" }
+  let(:tag) { Navigator::Tag.new "li", "Example text.", class: "example" }
 
   describe "#initialize" do
     it "raises ArgumentError when no name is supplied" do
@@ -23,6 +23,69 @@ describe Navigator::Tag do
       tag = Navigator::Tag.new "li", nil, class: "tooltip", "data-enabled" => true
       expect(tag.prefix).to eq('<li class="tooltip" data-enabled="true">')
     end
+
+    context "active default attribute" do
+      it "adds target value when search and target values match" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"}, {search_value: "/some/path"}
+        expect(tag.prefix).to eq(%(<a href="/some/path" class="active">))
+      end
+
+      it "adds target value when search (symbol key) and target (string key) values match" do
+        tag = Navigator::Tag.new "a", nil, {"href" => "/some/path"}, {search_value: "/some/path"}
+        expect(tag.prefix).to eq(%(<a href="/some/path" class="active">))
+      end
+
+      it "adds target value when search (string key) and target (symbol key) values match" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"}, {"search_value" => "/some/path"}
+        expect(tag.prefix).to eq(%(<a href="/some/path" class="active">))
+      end
+
+      it "adds target value when search key value (string) and target key (symbol) match" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"},
+                                           {"search_key" => "href", "search_value" => "/some/path"}
+
+        expect(tag.prefix).to eq(%(<a href="/some/path" class="active">))
+      end
+
+      it "appends target value when search and target values match" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path", class: "example"}, {search_value: "/some/path"}
+        expect(tag.prefix).to eq(%(<a href="/some/path" class="example active">))
+      end
+
+      it "doesn't add target value when search value isn't present" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"}
+        expect(tag.prefix).to eq(%(<a href="/some/path">))
+      end
+
+      it "doesn't add target value when target key isn't present" do
+        tag = Navigator::Tag.new "a", nil, {}, {search_value: "/some/path"}
+        expect(tag.prefix).to eq(%(<a>))
+      end
+
+      it "doesn't add target value when search and target values don't match" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"}, {search_value: "/some/other/path"}
+        expect(tag.prefix).to eq(%(<a href="/some/path">))
+      end
+    end
+
+    context "active custom attribute" do
+      it "adds target value for custom search key" do
+        tag = Navigator::Tag.new "span", nil, {"data-url" => "/some/path"},
+                                              {search_key: "data-url", search_value: "/some/path"}
+
+        expect(tag.prefix).to eq(%(<span data-url="/some/path" class="active">))
+      end
+
+      it "adds target value for custom target key" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"}, {search_value: "/some/path", target_key: "data-id"}
+        expect(tag.prefix).to eq(%(<a href="/some/path" data-id="active">))
+      end
+
+      it "adds target value for custom target value" do
+        tag = Navigator::Tag.new "a", nil, {href: "/some/path"}, {search_value: "/some/path", target_value: "custom"}
+        expect(tag.prefix).to eq(%(<a href="/some/path" class="custom">))
+      end
+    end
   end
 
   describe "#suffix" do
@@ -43,7 +106,7 @@ describe Navigator::Tag do
     end
 
     it "renders a tag with attributes and content" do
-      expect(tag.render).to eq('<li class="example">Not much here.</li>')
+      expect(tag.render).to eq('<li class="example">Example text.</li>')
     end
   end
 end
