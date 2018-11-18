6ec503a21be1e435a8e95df302603dcdbdc415c4
Brooke Kuhlmann <brooke@alchemists.io>
2018-11-17 18:03:50 -0700

    Fixed Rubocop Lint/Void issues.
    These are false positives and more an issue with Rubocop itself.

--- spec/lib/navigator/menu_spec.rb
+++ spec/lib/navigator/menu_spec.rb
@@ -441,10 +441,12 @@ RSpec.describe Navigator::Menu do
       expect(menu.render).to eq("<ul><strong>bold</strong></ul>")
     end
 
+    # rubocop:disable Lint/Void
     it "adds sub tag" do
       menu.sub "sub-text"
       expect(menu.render).to eq("<ul><sub>sub-text</sub></ul>")
     end
+    # rubocop:enable Lint/Void
 
     it "adds sup tag" do
       menu.sup "super-text"
@@ -461,10 +463,12 @@ RSpec.describe Navigator::Menu do
       expect(menu.render).to eq("<ul><label>label</label></ul>")
     end
 
+    # rubocop:disable Lint/Void
     it "adds select tag" do
       menu.select
       expect(menu.render).to eq("<ul><select></select></ul>")
     end
+    # rubocop:enable Lint/Void
 
     it "adds option tag" do
       menu.option
