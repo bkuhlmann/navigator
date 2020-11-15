d39d083c6f1bc3c91a7243abacfab3aec1506b00
Brooke Kuhlmann <brooke@alchemists.io>
2020-11-14 17:57:03 -0700

    Added version release notes

--- lib/navigator/identity.rb
+++ lib/navigator/identity.rb
@@ -5,7 +5,7 @@ module Navigator
   module Identity
     NAME = "navigator"
     LABEL = "Navigator"
-    VERSION = "5.3.0"
+    VERSION = "5.4.0"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
   end
 end
