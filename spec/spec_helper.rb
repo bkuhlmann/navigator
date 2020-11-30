49ea148b22cd473f4db246af7bf9038e44e5b4ef
Brooke Kuhlmann <brooke@alchemists.io>
2020-11-29 18:15:12 -0700

    Added Refinements development dependency
    Provides enhancements to the Ruby core libraries and, in some cases,
    performance improvements too. Initially, this library will be used for
    development and testing purposes but might be converted to an
    implementation dependency at some point in the future.
    
    [Details](https://www.alchemists.io/projects/refinements/changes.html).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,12 @@ SimpleCov.start { enable_coverage :branch }
 
 require "pry"
 require "pry-byebug"
+require "refinements"
 
-Dir[File.join(__dir__, "support", "shared_contexts", "**/*.rb")].sort.each { |path| require path }
-Dir[File.join(__dir__, "support", "shared_examples", "**/*.rb")].sort.each { |path| require path }
+using Refinements::Pathnames
+
+Pathname.require_tree __dir__, "support/shared_contexts/**/*.rb"
+Pathname.require_tree __dir__, "support/shared_examples/**/*.rb"
 
 RSpec.configure do |config|
   config.color = true
