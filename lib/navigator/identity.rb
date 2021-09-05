e9d3c6a91104722d4274d0d59e6784032762105b
Brooke Kuhlmann <brooke@alchemists.io>
2021-09-05 10:23:48 -0600

    Fixed Rubocop Style/MutableConstant issue

--- lib/navigator/identity.rb
+++ lib/navigator/identity.rb
@@ -6,6 +6,6 @@ module Navigator
     NAME = "navigator"
     LABEL = "Navigator"
     VERSION = "6.0.2"
-    VERSION_LABEL = "#{LABEL} #{VERSION}"
+    VERSION_LABEL = "#{LABEL} #{VERSION}".freeze
   end
 end
